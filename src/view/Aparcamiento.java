708baef1b5f4ebb38ec5d2c96f447a2e66b231b8
mpineirotroncoso <145323703+mpineirotroncoso@users.noreply.github.com>
2024-11-21 11:52:48 +0100

    Eliminar JLabel innecesaria

--- src/view/Aparcamiento.java
+++ src/view/Aparcamiento.java
@@ -9,8 +9,8 @@ import java.util.List;
 public class Aparcamiento extends JPanel {
     public Aparcamiento(List<LabelPlaza> plazas, CartelLleno cartelLleno) {
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-        JLabel label = new JLabel("Aparcamiento");
-        add(label);
+        //JLabel label = new JLabel("Aparcamiento");
+        //add(label);
         for (int i = 0; i < 10; i++) {
             System.out.println(plazas);
             String matricula = ParkingController.getMatriculaFromPlaza(i);
